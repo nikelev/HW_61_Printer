dc289f13aad06b11050f69d78ae159f9cc156c5a
nikelev <nikelev777@gmail.com>
2023-12-07 09:18:20 +0100

    commit 3

--- src/Printer.java
+++ src/Printer.java
@@ -1,24 +1,23 @@
 public class Printer extends Thread {
-    private int number;
-    Thread nextThread;
+    private int number; // Номер принтера
+    Thread nextThread; // Ссылка на следующий поток
 
-    public Printer(int number) {
-        this.number = number;
+    public Printer(int number) { // Конструктор класса
+        this.number = number; // Инициализация номера принтера
     }
 
-    public void setNextNumber(Thread nextThread) {
+    public void setNextNumber(Thread nextThread) { // Установка следующего потока
         this.nextThread = nextThread;
     }
 
-
-    public void run() {
-        for (int i = 0; i < 10; i++) {
+    public void run() { // Метод, который выполняется при запуске потока
+        for (int i = 0; i < 10; i++) { // Цикл выполняется 10 раз
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(String.valueOf(number).repeat(10));
+                Thread.sleep(1000); // Поток "спит" в течение одной секунды
+            } catch (InterruptedException e) { // Если поток прерывается во время сна
+                System.out.println(String.valueOf(number+1).repeat(10)); // Печатает строку, состоящую из номера принтера, повторенного 10 раз
                 if (nextThread != null) {
-                    nextThread.interrupt();
+                    nextThread.interrupt(); // Прерывает следующий поток, если он существует
                 }
             }
         }
--- src/PrinterAppl.java
+++ src/PrinterAppl.java
@@ -1,26 +1,20 @@
 public class PrinterAppl {
-    public static void main(String[] args) {
-        Printer[] printers = new Printer[4];
+    public static void main(String[] args) { // Точка входа в программу
+        Printer[] printers = new Printer[4]; // Создает массив из четырех принтеров
 
-        for (int i = 0; i < printers.length; i++) {
+        for (int i = 0; i < printers.length; i++) { // Инициализирует принтеры
             printers[i] = new Printer(i);
         }
 
-
-
-        for (int i = 0; i < printers.length; i++) {
+        for (int i = 0; i < printers.length; i++) { // Устанавливает для каждого принтера следующий принтер
             int nextIndex = (i + 1) % printers.length;
             printers[i].setNextNumber(printers[nextIndex]);
         }
 
-
-
-        for (Printer printer : printers) {
+        for (Printer printer : printers) { // Запускает все принтеры
             printer.start();
         }
 
-
-
-        printers[0].interrupt();
+        printers[0].interrupt(); // Прерывает первый принтер, начиная цепочку прерываний
     }
 }
